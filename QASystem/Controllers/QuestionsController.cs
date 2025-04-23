c3358c9eb40bc96424e7406d78d7c063af7d6ddd
nishikatakagi <quocviet242003@gmail.com>
2025-04-23 16:22:38 +0700

    feat: add create question view

---
+++ QASystem/Controllers/QuestionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QASystem.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
