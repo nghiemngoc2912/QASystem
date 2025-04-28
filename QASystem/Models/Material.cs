cc6aa7ee9b80f2db7bb7922d76e3bc77871143c3
AnhDuc77 <anhto7777777@gmail.com>
2025-04-29 02:27:49 +0700

    signalR for material, index material for all user

--- QASystem/Models/Material.cs
+++ QASystem/Models/Material.cs
@@ -1,9 +1,16 @@
-﻿namespace QASystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QASystem.Models
 {
     public class Material
     {
         public int MaterialId { get; set; }
+        [Required(ErrorMessage = "Title is required.")]
+        [StringLength(100, ErrorMessage = "Title cannot be longer than 100 characters.")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Description is required.")]
+        [StringLength(500, ErrorMessage = "Description cannot be longer than 500 characters.")]
         public string Description { get; set; }
         public string FileLink { get; set; }
         public int Downloads { get; set; }
